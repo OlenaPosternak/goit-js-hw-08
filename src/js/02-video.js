fef547e9a62214f4d1bd0a5b2f7c25d24e7143b2
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-08-05 00:46:38 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,29 +4,14 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const onPlay = function(seconds) {
-    
-        console.log(seconds);
+   console.log(`start`, seconds);
    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
-   
-
 };
-player.on('play', onPlay);
+
+player.on('timeupdate', onPlay);
 
 const newTime = localStorage.getItem("videoplayer-current-time");
 
-console.log(JSON.parse(newTime));
+console.log(`skip`, newTime);
 
-
-player.setCurrentTime(JSON.parse(newTime)).then(function(seconds) {
-    seconds = JSON.parse(newTime);
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(JSON.parse(newTime));
