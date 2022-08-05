ebe1791402484fb39b08e470b3f9e07e8aaf8224
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-08-05 19:55:44 +0300

    alert

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,36 +7,51 @@ const form = document.querySelector(`form`);
 form.addEventListener(`input`, throttle(onInputChange,500));
 form.addEventListener(`submit`, onFormSubmit);
 
+const STORAGE_KEY= `feedback-form-state`;
 const formData={};
 
 onPutSavedInfo ()
 
 function onInputChange(event){
    formData[event.target.name]=event.target.value
-   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 
 function onFormSubmit(event){
-    event.preventDefault();
-    // очищаем форму после отправки
-    event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    console.log(formData);
-
+    if (!textarea.value || !email.value){
+        alert(`Please fill in all the fields`)
+    }
+    else {
+        event.preventDefault();
+    
+        // очищаем форму после отправки
+        console.log(formData);
+        event.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+     
+    
+    }
+    
 };
 
 function onPutSavedInfo (){
-    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-    for (const key in data) {
-        // Если это собственное свойство - выполняем тело if
-        if (data.hasOwnProperty(key)) {
-        console.log(data[key]);
-        textarea.value = data.message  || ``;
-        email.value= data.email || ``;     
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (data){
+        for (const key in data) {
+            // Если это собственное свойство - выполняем тело if
+            if (data.hasOwnProperty(key)) {
+            // console.log(data[key]);
+            textarea.value = data.message  || ``;
+            email.value= data.email || ``;     
+            }
         }
+
     }
+
+    
       
+
 }
 
